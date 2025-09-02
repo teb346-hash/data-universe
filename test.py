e99286207b422402d48d82e61115a3dfcc399ff0
teb346-hash <teb346@gmail.com>
2025-09-02 09:54:40 +0200

    create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+print("Pandas Version is : " + pd.__version__)
+print(myvar)
